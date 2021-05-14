08f27d0a3f1e83a319b9eee0815feac7f967d29e
maisaa <maisaa.alkhedr@gmail.com>
2021-05-14 18:05:21 +0200

    get_All_Articles

--- main.js
+++ main.js
@@ -2,8 +2,52 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-console.log("hiiiiii")
 
-app.listen(port, ()=>{
+const articlesRouter = express.Router();
+
+
+app.use(express.json());
+app.use("/articles", articlesRouter);
+
+//get All Articles
+articlesRouter.get("/",(req,res,next)=>{
+    if(articles.length<=0){
+        const err = new Error("no articles");
+        err.status = 404;
+        next(err);
+    }
+    res.status(200);
+    res.json(articles);
+});
+
+
+const articles = [
+    {
+        id: 1,
+        title: 'How I learn coding?',
+        description:
+            'Lorem, Quam, mollitia.',
+        author: 'Jouza',
+    },
+    {
+        id: 2,
+        title: 'Coding Best Practices',
+        description:
+            'Lorem, ipsum dolor sit, Quam, mollitia.',
+        author: 'Besslan',
+    },
+    {
+        id: 3,
+        title: 'Debugging',
+        description:
+            'Lorem, Quam, mollitia.',
+        author: 'Jouza',
+    },
+];
+
+
+
+
+app.listen(port, () => {
     console.log(`server is running at http://localhost:${port}`)
 });
