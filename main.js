bd0062140e025bde45efec90c65a805efbbdc878
maisaa <maisaa.alkhedr@gmail.com>
2021-05-14 21:07:06 +0200

    getArticleById

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ app.use(express.json());
 app.use("/articles", articlesRouter);
 
 //get All Articles
-articlesRouter.get("/",(req,res,next)=>{
-    if(articles.length<=0){
+articlesRouter.get("/", (req, res, next) => {
+    if (articles.length <= 0) {
         const err = new Error("no articles");
         err.status = 404;
         next(err);
@@ -19,6 +19,36 @@ articlesRouter.get("/",(req,res,next)=>{
     res.status(200);
     res.json(articles);
 });
+
+articlesRouter.get("/search_2/:id", (req, res, next) => {
+    let index;
+    const articleId = req.params.id;//query
+    const found = articles.find((ele, i) => {
+        index = i;
+        // if(ele.id === articleId)
+        return ele.id == Number (articleId)
+    });
+    if (found) {
+        res.status(200);
+        console.log("articleId....", (index) + 1)
+        res.json(articles[(index)]);
+    } else {
+        const err = new Error(`this id(${articleId}) not Found`);
+        err.status = 404;
+        next(err);
+    }
+})
+
+articlesRouter.use((err, req, res, next) => {
+    res.status(err.status);
+    res.json({
+        err: {
+            status: err.status,
+            message: err.message,
+        },
+    });
+});
+
 
 
 const articles = [
